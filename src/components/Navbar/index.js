6d8bd9eeea48b6b2e793d26cec410ab923f5e194
Anoop M D <anoop.md1421@gmail.com>
2023-04-04 19:07:58 +0530

    feat: navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,8 +5,24 @@ const Navbar = () => {
   return (
     <StyledWrapper className="w-full site-navbar">
       <div>
-        <header className="flex items-center justify-between py-4">
-          Navbar
+        <header className="flex justify-between items-center px-4 py-4 bg-white border-b border-gray-200">
+          <div className="flex items-center">
+            <h1 className="text-lg font-bold text-gray-800">uptime</h1>
+          </div>
+          <nav className="flex items-center">
+            <a
+              href="#"
+              className="ml-6 text-sm font-medium text-gray-800 hover:text-gray-900"
+            >
+              Environments
+            </a>
+            <a
+              href="#"
+              className="ml-6 text-sm font-medium text-gray-800 hover:text-gray-900"
+            >
+              Services
+            </a>
+          </nav>
         </header>
       </div>
     </StyledWrapper>
